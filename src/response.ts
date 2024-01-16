eca9cb635e10c965ed692b4f6f6630caaeda1cde
dojyorin <ota_droid@live.jp>
2024-01-17 02:26:33 +0900

    WIP.

--- src/response.ts
+++ src/response.ts
@@ -13,7 +13,7 @@ export function resRedirect(path:string){
     });
 }
 
-export function resContent(body:BodyInit, type:string, cors?:boolean){
+export function resContent(body:BodyInit, type:string, cors?:true){
     return new Response(body, {
         headers: {
             "Content-Type": type,
