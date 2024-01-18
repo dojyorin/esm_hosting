3f8a331536b8fb9990e865812ab8268add676682
dojyorin <ota_droid@live.jp>
2024-01-19 03:44:10 +0900

    fix shell.

--- script/start_local.sh
+++ script/start_local.sh
@@ -3,4 +3,4 @@ set -eu
 
 cd ${0%/*}
 
-deno run --allow-net --allow-read --allow-env $([[ -f ../.env ]] && echo '--env') $([[ "${1}" == '--unsafe' ]] && echo '--unsafely-ignore-certificate-errors') ../main.ts
+deno run --allow-net --allow-env --allow-read --allow-write $([[ -f ../.env ]] && echo '--env') $([[ ${1-''} == '--unsafe' ]] && echo '--unsafely-ignore-certificate-errors') ../main.ts
